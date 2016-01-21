2d1e1b49e3a9664fea3e3f2151e6def22841c1dc
mbb <bingb511@126.com>
2016-01-21 16:53:27 +0800

    0121

--- xml/bingb511/src/com/example/bingb511/IndexActivity.java
+++ xml/bingb511/src/com/example/bingb511/IndexActivity.java
@@ -1,8 +1,19 @@
 package com.example.bingb511;
 
+import java.io.FileNotFoundException;
+
 import android.app.TabActivity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
 @SuppressWarnings("deprecation")
@@ -18,6 +29,23 @@ public class IndexActivity extends TabActivity {
 		addTab(); // 添加标签
 		tabHost.setBackgroundColor(Color.argb(255, 255, 255, 1)); // 设置tabhost的颜色
 		tabHost.setBackgroundResource(R.drawable.bg_h); // 设置tabhost的背景图片
+		
+		//按钮显示图片
+		Button button = (Button)findViewById(R.id.b01);    
+        button.setText("选择图片");    
+        button.setOnClickListener(new Button.OnClickListener(){    
+            @Override    
+            public void onClick(View v) {    
+                Intent intent = new Intent();    
+                /* 开启Pictures画面Type设定为image */    
+                intent.setType("image/*");    
+                /* 使用Intent.ACTION_GET_CONTENT这个Action */    
+                intent.setAction(Intent.ACTION_GET_CONTENT);     
+                /* 取得相片后返回本画面 */    
+                startActivityForResult(intent, 1);    
+            }    
+                
+        });    
 	}
 
 	private void addTab() {
@@ -34,4 +62,22 @@ public class IndexActivity extends TabActivity {
 		tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("压缩图片", getResources().getDrawable(R.drawable.version))
 				.setContent(R.id.tab3));
 	}
-}
+
+	//按钮显示图片
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {    
+	    if (resultCode == RESULT_OK) {    
+	        Uri uri = data.getData();    
+	        Log.e("uri", uri.toString());    
+	        ContentResolver cr = this.getContentResolver();    
+	        try {    
+	            Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));    
+	            ImageView imageView = (ImageView) findViewById(R.id.iv01);    
+	            /* 将Bitmap设定到ImageView */    
+	            imageView.setImageBitmap(bitmap);    
+	        } catch (FileNotFoundException e) {    
+	            Log.e("Exception", e.getMessage(),e);    
+	        }    
+	    }    
+	    super.onActivityResult(requestCode, resultCode, data);    
+	 }    
+}   
